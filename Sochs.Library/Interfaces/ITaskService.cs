69aee35b84ae13d73cbbf0b169c27f84f5b17178
Ryan Whitwell <ryanwhitwell.developer@gmail.com>
2023-08-29 12:35:31 -0400

    Declares interface for TaskService

---
+++ Sochs.Library/Interfaces/ITaskService.cs
@@ -0,0 +1,16 @@
+﻿using Sochs.Library.Events;
+using Sochs.Library.Models;
+
+namespace Sochs.Library.Interfaces
+{
+  public interface ITaskService
+  {
+    event EventHandler<TimeOfDayTasksChangeEventArgs>? OnTimeOfDayTasksChange;
+
+    event EventHandler<DailyTasksResetEventArgs>? OnDailyTasksReset;
+
+    event EventHandler<TaskUpdatedEventArgs>? OnTaskUpdated;
+
+    public IEnumerable<DailyTask> ActiveTasks { get; }
+  }
+}
---
+++ Sochs.Library/Models/DailyTask.cs
@@ -0,0 +1,9 @@
+﻿namespace Sochs.Library.Models
+{
+  public class DailyTask
+  {
+    public string Description { get; set; } = string.Empty;
+
+    public string ImagePath { get; set; } = string.Empty;
+  }
+}
