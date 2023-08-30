4a3fb4bf71d4d0fd1e7bdd97ea649abaeca5fb52
Ryan Whitwell <ryanwhitwell.developer@gmail.com>
2023-08-30 11:47:17 -0400

    Adds support for achievement events

---
+++ Sochs.Library/Events/DailyTaskAchievementEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using Sochs.Library.Enums;
+using Sochs.Library.Models;
+
+namespace Sochs.Library.Events
+{
+  public class DailyTaskAchievementEventArgs : EventArgs
+  {
+    public Child Child { get; set; }
+
+    public DailyTaskData? TaskData { get; set; }
+  }
+}
